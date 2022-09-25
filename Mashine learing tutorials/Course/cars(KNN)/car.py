dccc2edb2748e7bdd7b4ba9480b391ac08ee20d2
Pepesob <pepesob2@gmail.com>
2022-09-25 18:44:13 +0200

    Commiting some new stuff

---
+++ Mashine learing tutorials/Course/cars(KNN)/car.py
@@ -0,0 +1,43 @@
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+
+col_names = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
+
+data = pd.read_csv("car.data", names=col_names)
+
+
+le = preprocessing.LabelEncoder()
+
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+doors = le.fit_transform(list(data["doors"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["class"]))
+
+predict = "class"
+
+i = list(zip(buying, maint, doors, persons, lug_boot, safety))
+y = list(cls)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(i, y, test_size=0.1)
+
+model = KNeighborsClassifier(n_neighbors=7)
+
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ['unacc', 'acc', 'good', 'vgood']
+
+for i in range(len(x_test)):
+    print("Predicted:", names[predicted[i]], "Data:", x_test[i], "Actual:", names[y_test[i]])
+    n = model.kneighbors([x_test[i]], 7, True)
+    print("N:", n)
