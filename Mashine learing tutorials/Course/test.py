4caccdab54a933a80b47dfb2ba4d4354e0b92cf2
Pepesob <pepesob2@gmail.com>
2022-09-26 00:39:49 +0200

    Commiting some new stuff

--- Mashine learing tutorials/Course/test.py
+++ Mashine learing tutorials/Course/test.py
@@ -1,18 +1,8 @@
+import numpy as np
 import sklearn
-from sklearn import datasets
-from sklearn import svm
+from sklearn.preprocessing import scale
+from sklearn.datasets import load_digits
+from sklearn.cluster import KMeans
 
-
-cancer = datasets.load_breast_cancer()
-
-print(cancer.feature_names)
-print(cancer.target_names)
-
-x = cancer.data
-y = cancer.target
-
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x,y,test_size=0.2)
-
-print(x_train,y_train)
-classes = ['malignant', 'benign']
-
+digits = load_digits()
+data = scale(digits.data)
