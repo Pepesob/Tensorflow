956b1f62e3d7150212e3dcb816efd4b6504d9513
Pepesob <pepesob@gmail.com>
2022-11-04 21:24:42 +0100

    update

---
+++ Mashine learing tutorials/My_models/my_model.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+f = tf.keras.datasets.fashion_mnist
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(64, (3,3),activation="relu",input_shape=(28,28,1)),
+    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.MaxPool2D((2, 2)),
+    tf.keras.layers.Conv2D(16, (3,3), activation="relu"),
+    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.MaxPool2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(32,activation="relu"),
+    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.Dense(10,activation="softmax"),
+])
+
+print(model.summary())
+
+
+model.compile(optimizer="adam",loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(x_train, y_train, epochs=5)
+
+test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
+
+print('\nTest accuracy:', test_acc, "\nTest loss:",test_loss)
+
+model.save("minst_digits_model")
